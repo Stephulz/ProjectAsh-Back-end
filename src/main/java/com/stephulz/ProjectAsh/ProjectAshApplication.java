dbe77e94c5cdd935eec57ee88926b465a607af89
Stefan Sanches <stefansanches9@gmail.com>
2018-11-01 13:58:27 -0300

    Spring initlzr

---
+++ src/main/java/com/stephulz/ProjectAsh/ProjectAshApplication.java
@@ -0,0 +1,12 @@
+package com.stephulz.ProjectAsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAshApplication.class, args);
+	}
+}
